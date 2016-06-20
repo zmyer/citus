f51884249868de92ab8523a34d0b67e35ab8d334
Brian Cloutier <github@briancloutier.com>
2016-06-20 18:10:27 +0300

    Add tests for RowCompareExpr and COALESCE

--- src/test/regress/sql/multi_function_evaluation.sql
+++ src/test/regress/sql/multi_function_evaluation.sql
@@ -30,9 +30,41 @@ SELECT * FROM example;
 ALTER TABLE example DROP value;
 ALTER TABLE example ADD value timestamp;
 
--- this is allowed because the planner strips away the CASE
+-- this is allowed because there are no mutable funcs in the CASE
+UPDATE example SET value = (CASE WHEN value > timestamp '12-12-1991' THEN timestamp '12-12-1991' ELSE value + interval '1 hour' END) WHERE key = 1;
+
+-- this is allowed because the planner strips away the CASE during constant evaluation
 UPDATE example SET value = CASE WHEN true THEN now() ELSE now() + interval '1 hour' END WHERE key = 1;
 
-UPDATE example SET value = (CASE WHEN now() > timestamp '12-12-1991' THEN now() ELSE now() + interval '1 hour' END) WHERE key = 1;
+-- this is not allowed because there're mutable functions in a CaseWhen clause
+-- (which we can't easily evaluate on the master)
+UPDATE example SET value = (CASE WHEN now() > timestamp '12-12-1991' THEN now() ELSE timestamp '10-24-1190' END) WHERE key = 1;
+
+-- make sure we also check defresult (the ELSE clause)
+UPDATE example SET value = (CASE WHEN now() > timestamp '12-12-1991' THEN timestamp '12-12-1191' ELSE now() END) WHERE key = 1;
+
+-- COALESCE is allowed
+UPDATE example SET value = COALESCE(null, null, timestamp '10-10-1000') WHERE key = 1;
+
+-- COALESCE is not allowed if there are any mutable functions
+UPDATE example SET value = COALESCE(now(), timestamp '10-10-1000') WHERE key = 1;
+UPDATE example SET value = COALESCE(timestamp '10-10-1000', now()) WHERE key = 1;
+
+-- RowCompareExpr's are checked for mutability. These are allowed:
+
+ALTER TABLE example DROP value;
+ALTER TABLE example ADD value boolean;
+ALTER TABLE example ADD time_col timestamptz;
+
+UPDATE example SET value = NULLIF(ROW(1, 2) < ROW(2, 3), true) WHERE key = 1;
+UPDATE example SET value = NULLIF(ROW(true, 2) < ROW(value, 3), true) WHERE key = 1;
+
+-- But this RowCompareExpr is not (it passes Var into STABLE)
+
+UPDATE example SET value = NULLIF(
+	ROW(date '10-10-1000', 2) < ROW(time_col, 3), true
+) WHERE key = 1;
 
 DROP TABLE example;
+
+-- Test RowCompareExpr?
